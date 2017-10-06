4709a0d141ce56a39c38b5717e2f8986d095c493
di-marcos <diego.marcos.gonzalez@gmail.com>
2017-10-06 10:42:51 +0200

    vectorized version of the inference

--- snake_inference_fast.py
+++ snake_inference_fast.py
@@ -21,29 +21,21 @@ def active_contour_step(step_n, Fu, Fv, du, dv,
     L = snake_u.shape[0]
     M = Fu.shape[0]
     N = Fu.shape[1]
-    s = 2 #Number of subdivision per edge to compute the balloon force
-    kappa_collection = np.zeros((L,s))
-    dEb_du = np.zeros((L,1))
-    dEb_dv = np.zeros((L,1))
-    u = np.int32(np.round(snake_u))
-    v = np.int32(np.round(snake_v))
+    s = 10 #Number of subdivision per edge to compute the balloon force
+
+
 
     # Explicit time stepping for image energy minimization:
 
-    a = np.zeros((L))
-    b = np.zeros((L))
-    fu = np.zeros((L))
-    fv = np.zeros((L))
     snake_hist = []
     for step in range(step_n):
-        for i in range(L):
-            a[i] = alpha[u[i,0],v[i,0]]
-            b[i] = beta[u[i,0], v[i,0]]
-            fu[i] = Fu[u[i,0], v[i,0]]
-            fv[i] = Fv[u[i,0], v[i,0]]
+        u = np.int16(snake_u)
+        v = np.int16(snake_v)
+        a = alpha[u,v].squeeze()
+        b = beta[u,v].squeeze()
+        fu = Fu[u,v]
+        fv = Fv[u,v]
 
-        fu = np.reshape(fu,(L,1))
-        fv = np.reshape(fv,(L,1))
         am1 = np.concatenate([a[L-1:L],a[0:L-1]],axis=0)
         a0d0 = np.diag(a)
         am1d0 = np.diag(am1)
@@ -67,59 +59,29 @@ def active_contour_step(step_n, Fu, Fv, du, dv,
         B = bm1dm2 - 2*(bm1dm1+b0dm1) + (bm1d0+4*b0d0+b1d0) - 2*(b0d1+b1d1) + b1d2
 
         #Get kappa values between nodes
-        for i in range(L):
-            next_i = i + 1
-            if next_i == L:
-                next_i = 0
-            u_interp = np.int32(snake_u[i]+range(s)*(snake_u[next_i]-snake_u[i])/s)
-            v_interp = np.int32(snake_v[i]+range(s)*(snake_v[next_i]-snake_v[i])/s)
-            for j in range(s):
-                kappa_collection[i,j] = kappa[u_interp[j], v_interp[j]]
+        u_interps = np.int16(snake_u + np.arange(s)*(snake_u[np.roll(np.arange(L), -1)]-snake_u)/s)
+        v_interps = np.int16(snake_v + np.arange(s)*(snake_v[np.roll(np.arange(L), -1)]-snake_v)/s)
+        kappa_collection = kappa[u_interps,v_interps]
 
 
-        #Get the derivative of the balloon energy
-        for i in range(L):
-            next_i = i + 1
-            prev_i = i - 1
-            if next_i == L:
-                next_i = 0
-            if prev_i == -1:
-                prev_i = L-1
-            val = 0
-            #contribution from the i+1 triangle to dE/du
-            int_end = snake_v[next_i] - snake_v[i]
-            dh = np.abs(int_end/s)
-            for j in range(s):
-                val += np.sign(int_end)*(j+1)/s * kappa_collection[i,s-j-1] * dh
-            #contribution from the i-1 triangle to dE/du
-            int_end = snake_v[prev_i] - snake_v[i]
-            dh = np.abs(int_end / s)
-            for j in range(s):
-                val += -np.sign(int_end)*(j+1)/s * kappa_collection[i,j] * dh
-            dEb_du[i] = val
-
-            val = 0
-            # contribution from the i+1 triangle to dE/dv
-            int_end = snake_u[next_i] - snake_u[i]
-            dh = np.abs(int_end / s)
-            for j in range(s):
-                val += -np.sign(int_end)*(j+1) / s  * kappa_collection[prev_i,s-j-1] * dh
-            # contribution from the i-1 triangle to dE/dv
-            int_end = snake_u[prev_i] - snake_u[i]
-            dh = np.abs(int_end / s)
-            for j in range(s):
-                val += np.sign(int_end)*(j+1) / s  * kappa_collection[prev_i,j] * dh
-            dEb_dv[i] = val
-
-
+        # Get the derivative of the balloon energy
+        js = np.arange(1, s + 1)
+        s2 = 1 / (s * s)
+        int_ends_u_next = s2 * (snake_u[np.roll(np.arange(L), -1)] - snake_u)  # snake_u[next_i] - snake_u[i]
+        int_ends_u_prev = s2 * (snake_u[np.roll(np.arange(L), 1)] - snake_u)  # snake_u[prev_i] - snake_u[i]
+        int_ends_v_next = s2 * (snake_v[np.roll(np.arange(L), -1)] - snake_v)  # snake_v[next_i] - snake_v[i]
+        int_ends_v_prev = s2 * (snake_v[np.roll(np.arange(L), 1)] - snake_v)  # snake_v[prev_i] - snake_v[i]
+        # contribution from the i+1 triangles to dE/du
+        dEb_du = np.sum(js*kappa_collection[:, np.arange(s - 1, -1, -1)],axis=1) * int_ends_v_next.squeeze()
+        dEb_du -= np.sum(js*kappa_collection[:, js - 1],axis=1) * int_ends_v_prev.squeeze()
+        dEb_dv = -np.sum(js * kappa_collection[np.roll(np.arange(L), 1),:][:, np.arange(s - 1, -1, -1)], axis=1) * int_ends_u_next.squeeze()
+        dEb_dv += np.sum(js * kappa_collection[np.roll(np.arange(L), 1),:][:, js - 1], axis=1) * int_ends_u_prev.squeeze()
 
 
         # Movements are capped to max_px_move per iteration:
-        du = -max_px_move*np.tanh( (fu - dEb_du + 2*np.matmul(A/delta_s+B/np.square(delta_s),snake_u))*gamma )*0.1 + du*0.9
-        dv = -max_px_move*np.tanh( (fv - dEb_dv + 2*np.matmul(A/delta_s+B/np.square(delta_s),snake_v))*gamma )*0.1 + dv*0.9
+        du = -max_px_move*np.tanh( (fu - dEb_du.reshape([L,1]) + 2*np.matmul(A/delta_s+B/np.square(delta_s),snake_u))*gamma )*0.1 + du*0.9
+        dv = -max_px_move*np.tanh( (fv - dEb_dv.reshape([L,1]) + 2*np.matmul(A/delta_s+B/np.square(delta_s),snake_v))*gamma )*0.1 + dv*0.9
 
-        #du += np.multiply(k,n_u)
-        #dv += np.multiply(k,n_v)
         #du += dEb_du
         #dv += dEb_dv
 
@@ -129,8 +91,6 @@ def active_contour_step(step_n, Fu, Fv, du, dv,
         snake_v = np.minimum(snake_v, np.float32(N)-1)
         snake_u = np.maximum(snake_u, 1)
         snake_v = np.maximum(snake_v, 1)
-        snake_u[L-1] = snake_u[0]
-        snake_v[L - 1] = snake_v[0]
         snake_hist.append(np.array([snake_u[:, 0], snake_v[:, 0]]).T)
 
     return snake_u,snake_v,du,dv,snake_hist

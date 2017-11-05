0d053fdc2565379d59dd94d32e262eb2a8922310
Diego Marcos <marcosdi@cluster57.ais.sandbox>
2017-11-05 17:15:09 -0500

    added val paths

--- TF_snakes_tcity_test.py
+++ TF_snakes_tcity_test.py
@@ -59,9 +59,9 @@ batch_size = 1
 im_size = 384
 out_size = 192
 if intoronto:
-    images_path = '/ais/dgx1/marcosdi/TCityBuildings/building_crops/'
-    gt_path = '/ais/dgx1/marcosdi/TCityBuildings/building_crops_gt/'
-    dwt_path = '/ais/dgx1/marcosdi/TCityBuildings/building_crops_dwt/'
+    images_path = '/ais/dgx1/marcosdi/TCityBuildings/val_building_crops/'
+    #gt_path = '/ais/dgx1/marcosdi/TCityBuildings/building_crops_gt/'
+    dwt_path = '/ais/dgx1/marcosdi/TCityBuildings/val_building_crops_dwt/'
 else:
     images_path = '/mnt/bighd/Data/TorontoCityTile/building_crops/'
     gt_path = '/mnt/bighd/Data/TorontoCityTile/building_crops_gt/'
@@ -91,11 +91,11 @@ csv_names = [f for f in files if f[-4:] == '.csv']
 png_names = [f for f in files if f[-4:] == '.png']
 total_num = len(png_names)
 images = np.zeros([im_size,im_size,3,epoch_batch_size],dtype=np.uint8)
-masks = np.zeros([out_size,out_size,1,epoch_batch_size],dtype=np.uint8)
-GT = np.zeros([L,2,epoch_batch_size])
+#masks = np.zeros([out_size,out_size,1,epoch_batch_size],dtype=np.uint8)
+#GT = np.zeros([L,2,epoch_batch_size])
 DWT = np.zeros([L,2,epoch_batch_size])
 
-allGT = np.zeros([L,2,total_num])
+#allGT = np.zeros([L,2,total_num])
 allDWT = np.zeros([L,2,total_num])
 
 all_building_names = []
@@ -108,25 +108,26 @@ for csv_name in csv_names:
     tile_name = csv_name[0:-7]
     print('Reading tile: '+ tile_name,flush=True)
     copyfile(dwt_path + tile_name + '_bb.csv',results_path + tile_name + '_bb.csv')
-    csvfile_gt = open(gt_path + tile_name + '_polygons.csv', newline='')
-    reader_gt = csv.reader(csvfile_gt)
+    #csvfile_gt = open(gt_path + tile_name + '_polygons.csv', newline='')
+    #reader_gt = csv.reader(csvfile_gt)
     csvfile_dwt = open(dwt_path + tile_name + '_polygons.csv', newline='')
     reader_dwt = csv.reader(csvfile_dwt)
     while True:
         try:
-            corners_gt = reader_gt.__next__()
+            #corners_gt = reader_gt.__next__()
             corners_dwt = reader_dwt.__next__()
         except:
             print('Buildings loaded: '+str(i)+', total: '+str(total_count),flush=True)
             break
         # Get GT polygons
-        num_points = np.int32(corners_gt[0])
-        poly = np.zeros([num_points, 2])
-        for c in range(num_points):
-            poly[c, 0] = np.float(corners_gt[1 + 2 * c]) * out_size / im_size
-            poly[c, 1] = np.float(corners_gt[2 + 2 * c]) * out_size / im_size
-        [tck, u] = interpolate.splprep([poly[:, 0], poly[:, 1]], s=2, k=1, per=1)
-        [allGT[:, 0, total_count], allGT[:, 1, total_count]] = interpolate.splev(np.linspace(0, 1, L), tck)
+        #num_points = np.int32(corners_gt[0])
+        #poly = np.zeros([num_points, 2])
+        #for c in range(num_points):
+        #    poly[c, 0] = np.float(corners_gt[1 + 2 * c]) * out_size / im_size
+        #    poly[c, 1] = np.float(corners_gt[2 + 2 * c]) * out_size / im_size
+        #[tck, u] = interpolate.splprep([poly[:, 0], poly[:, 1]], s=2, k=1, per=1)
+        #[allGT[:, 0, total_count], allGT[:, 1, total_count]] = interpolate.splev(np.linspace(0, 1, L), tck)
+        
         # Get DWT polygons
         num_points = np.int32(corners_dwt[0])
         poly = np.zeros([num_points, 2])
@@ -142,8 +143,8 @@ for csv_name in csv_names:
         i += 1
         total_count += 1
 
-allGT = np.minimum(allGT,out_size-1)
-allGT = np.maximum(allGT,0)
+#allGT = np.minimum(allGT,out_size-1)
+#allGT = np.maximum(allGT,0)
 allDWT = np.minimum(allDWT,out_size-1)
 allDWT = np.maximum(allDWT,0)
 
@@ -158,9 +159,9 @@ def resample_images(inds):
     for i in range(len(inds)):
         this_im = scipy.misc.imread(images_path + all_building_names[inds[i]])
         images[:, :, :, i] = scipy.misc.imresize(this_im, [im_size, im_size])
-        this_mask = scipy.misc.imread(gt_path + all_building_names[inds[i]])
-        masks[:, :, 0, i] = scipy.misc.imresize(this_mask, [out_size, out_size], interp='nearest') > 0
-        GT[:,:,i] = allGT[:,:,inds[i]]
+        #this_mask = scipy.misc.imread(gt_path + all_building_names[inds[i]])
+        #masks[:, :, 0, i] = scipy.misc.imresize(this_mask, [out_size, out_size], interp='nearest') > 0
+        #GT[:,:,i] = allGT[:,:,inds[i]]
         DWT[:, :, i] = allDWT[:, :, inds[i]]
         building_names.append(all_building_names[inds[i]])
 
@@ -196,7 +197,7 @@ def epoch(n,i,mode):
     # mode (str): train or test
     batch_ind = np.arange(i,i+batch_size)
     batch = np.float32(images[:, :, :, batch_ind])/255
-    batch_mask = np.copy(masks[:, :, :, batch_ind])
+    #batch_mask = np.copy(masks[:, :, :, batch_ind])
     thisNames = building_names[batch_ind[0]]
     thisGT = np.copy(GT[:, :, batch_ind[0]])
     thisDWT = np.copy(DWT[:, :, batch_ind[0]])
@@ -214,30 +215,30 @@ def epoch(n,i,mode):
         der1, der2 = derivatives_poly(snake)
 
 
-        der1_GT, der2_GT = derivatives_poly(thisGT)
+        #der1_GT, der2_GT = derivatives_poly(thisGT)
 
-        grads_arrayE = mapE * 0.001
-        grads_arrayA = mapA * 0.001
-        grads_arrayB = mapB * 0.001
-        grads_arrayK = mapK * 0.001
-        grads_arrayE[:, :, 0, 0] -= draw_poly(snake, 1, [M, N],12) - draw_poly(thisGT, 1, [M, N],12)
-        grads_arrayA[:, :, 0, 0] -= (np.mean(der1) - np.mean(der1_GT))
-        grads_arrayB[:, :, 0, 0] -= (draw_poly(snake, der2, [M, N],12) - draw_poly(thisGT, der2_GT, [M, N],12))
-        mask_gt = draw_poly_fill(thisGT, [M, N])
+        #grads_arrayE = mapE * 0.001
+        #grads_arrayA = mapA * 0.001
+        #grads_arrayB = mapB * 0.001
+        #grads_arrayK = mapK * 0.001
+        #grads_arrayE[:, :, 0, 0] -= draw_poly(snake, 1, [M, N],12) - draw_poly(thisGT, 1, [M, N],12)
+        #grads_arrayA[:, :, 0, 0] -= (np.mean(der1) - np.mean(der1_GT))
+        #grads_arrayB[:, :, 0, 0] -= (draw_poly(snake, der2, [M, N],12) - draw_poly(thisGT, der2_GT, [M, N],12))
+        #mask_gt = draw_poly_fill(thisGT, [M, N])
         mask_snake = draw_poly_fill(snake, [M, N])
-        grads_arrayK[:, :, 0, 0] -= mask_gt - mask_snake
+        #grads_arrayK[:, :, 0, 0] -= mask_gt - mask_snake
 
-        intersection = (mask_gt+mask_snake) == 2
-        union = (mask_gt + mask_snake) >= 1
-        iou = np.sum(intersection) / np.sum(union)
+        #intersection = (mask_gt+mask_snake) == 2
+        #union = (mask_gt + mask_snake) >= 1
+        #iou = np.sum(intersection) / np.sum(union)
 
-    if do_plot and n >=3:
-        plot_snakes(snake, snake_hist, thisGT, mapE, np.maximum(mapA, 0), np.maximum(mapB, 0), mapK, \
+    #if do_plot and n >=3:
+    #    plot_snakes(snake, snake_hist, thisGT, mapE, np.maximum(mapA, 0), np.maximum(mapB, 0), mapK, \
                 grads_arrayE, grads_arrayA, grads_arrayB, grads_arrayK, batch, batch_mask)
         #plt.show()
     if do_write_results:
         scipy.misc.imsave(results_path+thisNames,scipy.misc.imresize(mask_snake,[im_size,im_size],interp='nearest'))
-    return iou
+    return
 
 
 ###########################################################################################
@@ -261,11 +262,10 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,log_device_place
         iter_count = 0
 
         for i in range(len(inds)):
-            iou_test += epoch(n,i, 'test')
+            #iou_test += epoch(n,i, 'test')
             iter_count += 1
             total_iter_count += 1
-            print('Test. Batch ' + str(n) + '. Iter ' + str(total_iter_count) + '/' + str(total_num) + ', IoU = %.4f' % (
-            iou_test / total_iter_count),flush=True)
+            print('Test. Batch ' + str(n) + '. Iter ' + str(total_iter_count) + '/' + str(total_num),flush=True)
 
 
 

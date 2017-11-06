97e69b61e95b789df12bf44b784d482dcd908b2c
diego <diego.marcos.gonzalez@gmail.com>
2017-11-06 17:08:29 +0100

    changed reconstitute crops

--- reconstitute_crops.m
+++ reconstitute_crops.m
@@ -1,4 +1,4 @@
-intoronto = true;
+intoronto = false;
 
 crops_path = 'building_crops_gt';
 result_path = 'result_binary';
@@ -23,10 +23,9 @@ for num = 1:numel(bb_names)
     crop_names = dir(fullfile(crops_path,[imname,'*.png']));
     for i = 1:size(bb,1)
         crop = imread(fullfile(crops_path,crop_names(i).name));
+        crop = crop(:,:,1);
         crop = imresize(crop,bb(i,[4 3]),'nearest');
         prev_crop = im(bb(i,2):bb(i,2)+bb(i,4)-1,bb(i,1):bb(i,1)+bb(i,3)-1);
-        disp(size(crop));
-        disp(size(prev_crop));
         prev_crop(crop(:)>0) = i;
         im(bb(i,2):bb(i,2)+bb(i,4)-1,bb(i,1):bb(i,1)+bb(i,3)-1) = prev_crop;
     end

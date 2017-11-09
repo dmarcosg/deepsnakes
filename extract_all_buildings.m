b5f0b7d9a9880906b9d89b66918dccdf95ee55cf
diego <diego.marcos.gonzalez@gmail.com>
2017-11-09 21:13:26 +0100

    ready to continue training model B

--- extract_all_buildings.m
+++ extract_all_buildings.m
@@ -37,6 +37,7 @@ end
 if training
     ims = dir(fullfile(ims_path,'*_0.png'));
     gts = dir(fullfile(gt_path,'*_instances.png'));
+    gts = [gts,dir(fullfile(gt_path,'*_labels.png'))];
     dwts = dir(fullfile(dwt_path,'*_binary.png'));
 else
     ims = dir(fullfile(ims_path,'*.png'));
@@ -88,9 +89,11 @@ for num = 1:numel(ims)
             disp(['Couldnt read ',fullfile(gt_path,gts(num).name)]);
             continue;
         end
-        %building_map = gt(:,:,1)==255 & gt(:,:,2) == 0 & gt(:,:,3) == 0;
-        %building_map = gt(:,:,1) > 0;
-        %building_map = bwlabel(building_map);
+        if size(gt,3) == 3
+            building_map = gt(:,:,1)==255 & gt(:,:,2) == 0 & gt(:,:,3) == 0;
+            building_map = gt(:,:,1) > 0;
+            building_map = bwlabel(building_map);
+        end
         building_map = gt;
         %stats_gt = regionprops('table',building_map,building_map,'Centroid','BoundingBox','MeanIntensity');
         %assert(sum((stats_gt{1:end,'MeanIntensity'}' ~= (1:size(stats_gt,1))))==0);
